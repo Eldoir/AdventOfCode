f294121a604da51368e746afec61fc7d72b4cbd0
Arthur Cousseau <arthur.cousseau@me.com>
2021-12-08 20:38:35 +0100

    Add Problem

---
+++ AdventOfCode/Problem.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace AdventOfCode
+{
+    abstract class Problem
+    {
+        public abstract int Year { get; }
+        public abstract int Number { get; }
+
+        public string Text { get; private set; }
+        public string[] Lines { get; private set; }
+
+        public void Init()
+        {
+            string filepath = $"input/{Year}/{Number}.txt";
+            Text = File.ReadAllText(filepath);
+            Lines = File.ReadAllLines(filepath);
+
+            InitInternal();
+        }
+
+        protected virtual void InitInternal() { }
+
+        public abstract void ExecuteFirstStar();
+        public abstract void ExecuteSecondStar();
+    }
+}
