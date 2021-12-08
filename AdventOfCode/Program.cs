c8f3b74efec93c39d3cfddde06e9fc993568242e
Arthur Cousseau <arthur.cousseau@me.com>
2021-12-08 22:19:56 +0100

    Problem_2015_12 + import Newtonsoft.Json

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode
     {
         static void Main()
         {
-            var prob = new Problem_2015_11();
+            var prob = new Problem_2015_12();
             prob.Init();
             prob.Run();
         }
