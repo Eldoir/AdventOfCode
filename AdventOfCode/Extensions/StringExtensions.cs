07a24171a4f3d088410d9c6afe23f08164302c3c
Arthur Cousseau <arthur.cousseau@me.com>
2021-12-08 20:31:36 +0100

    Add Extensions

---
+++ AdventOfCode/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+namespace AdventOfCode.Extensions
+{
+    static class StringExtensions
+    {
+        public static int NbOccurrences(this string str, char c)
+        {
+            int total = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c) total++;
+            }
+
+            return total;
+        }
+    }
+}
