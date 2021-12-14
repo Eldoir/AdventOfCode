0cf5560585063a42e684d73cfd37793644f739f0
Arthur Cousseau <arthur.cousseau@me.com>
2021-12-14 20:18:45 +0100

    Problem_2021_2

---
+++ AdventOfCode/2021/Problem_2021_2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+    class Problem_2021_2 : Problem_2021
+    {
+        enum Direction
+        {
+            None = 0,
+            Forward,
+            Up,
+            Down
+        }
+
+        public override int Number => 2;
+
+        private List<KeyValuePair<Direction, int>> directions;
+
+        private int horizontal;
+        private int depth;
+
+        protected override void InitInternal()
+        {
+            directions = new List<KeyValuePair<Direction, int>>();
+
+            foreach (string line in Lines)
+            {
+                string[] split = line.Split(' ');
+                directions.Add(new KeyValuePair<Direction, int>(ToDirection(split[0]), int.Parse(split[1])));
+            }
+
+            base.InitInternal();
+        }
+
+        public override void Run()
+        {
+            foreach (var kvp in directions)
+            {
+                switch (kvp.Key)
+                {
+                    case Direction.Forward:
+                        horizontal += kvp.Value;
+                        break;
+                    case Direction.Up:
+                        depth -= kvp.Value;
+                        break;
+                    case Direction.Down:
+                        depth += kvp.Value;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"First star: {horizontal * depth}");
+
+            horizontal = 0;
+            depth = 0;
+
+            int aim = 0;
+
+            foreach (var kvp in directions)
+            {
+                switch (kvp.Key)
+                {
+                    case Direction.Forward:
+                        horizontal += kvp.Value;
+                        depth += aim * kvp.Value;
+                        break;
+                    case Direction.Up:
+                        aim -= kvp.Value;
+                        break;
+                    case Direction.Down:
+                        aim += kvp.Value;
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Second star: {horizontal * depth}");
+        }
+
+        #region Methods
+
+        private Direction ToDirection(string s)
+        {
+            switch (s)
+            {
+                case "forward": return Direction.Forward;
+                case "up": return Direction.Up;
+                case "down": return Direction.Down;
+                default: return Direction.None;
+            }
+        }
+
+        #endregion
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode
     {
         static void Main()
         {
-            var prob = new Problem_2021_1();
+            var prob = new Problem_2021_2();
             prob.Init();
             prob.Run();
         }
