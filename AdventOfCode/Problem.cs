590851b5823560e8c0d86c99dec4c02da921963d
Arthur Cousseau <arthur.cousseau@me.com>
2021-12-16 12:16:10 +0100

    Add support for test input

--- AdventOfCode/Problem.cs
+++ AdventOfCode/Problem.cs
@@ -7,16 +7,48 @@ namespace AdventOfCode
         public abstract int Year { get; }
         public abstract int Number { get; }
 
-        public string Text { get; private set; }
-        public string[] Lines { get; private set; }
+        public string Text => useTestInput ? textTest : text;
+        public string[] Lines => useTestInput ? linesTest : lines;
+
+        private string text;
+        private string[] lines;
+
+        private string textTest;
+        private string[] linesTest;
+
+        private bool useTestInput = false;
 
         public void Init()
         {
-            string filepath = $"../../../input/{Year}/{Number}.txt";
-            Text = File.ReadAllText(filepath);
-            Lines = File.ReadAllLines(filepath);
+            (text, lines) = InitTextAndLines(isTestInput: false);
+
+            if (TestFileExists())
+            {
+                (textTest, linesTest) = InitTextAndLines(isTestInput: true);
+            }
 
             InitInternal();
+        }
+
+        private (string, string[]) InitTextAndLines(bool isTestInput)
+        {
+            string inputFilePath = GetInputFilePath(isTestInput);
+            return (File.ReadAllText(inputFilePath), File.ReadAllLines(inputFilePath));
+        }
+
+        private bool TestFileExists()
+        {
+            return File.Exists(GetInputFilePath(isTestInput: true));
+        }
+
+        private string GetInputFilePath(bool isTestInput)
+        {
+            return $"../../../input/{Year}/{Number}{(isTestInput ? "_test" : "")}.txt";
+        }
+
+        protected void UseTestInput()
+        {
+            useTestInput = true;
         }
 
         protected virtual void InitInternal() { }
