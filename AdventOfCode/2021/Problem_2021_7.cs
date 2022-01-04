e159244796988f9c62670b59362aad3cdde8d0f5
Arthur Cousseau <arthur.cousseau@me.com>
2022-01-04 16:40:35 +0100

    Problem_2021_7: Second star

--- AdventOfCode/2021/Problem_2021_7.cs
+++ AdventOfCode/2021/Problem_2021_7.cs
@@ -7,12 +7,22 @@ namespace AdventOfCode
     {
         public override int Number => 7;
 
+        public delegate int CalcMethod(int baseNb, int nb);
+
         public override void Run()
         {
             int[] nbs = Text.Split(',').Select(l => int.Parse(l)).ToArray();
+
+            Console.WriteLine($"First star: {Process(nbs, CalcMethodFirstStar)}");
+            Console.WriteLine($"Second star: {Process(nbs, CalcMethodSecondStar)}");
+        }
+
+        #region Methods
+
+        static int Process(int[] nbs, CalcMethod methodCalc)
+        {
             int min = nbs.Min();
             int max = nbs.Max();
-
             int minDistance = int.MaxValue;
 
             for (int i = min; i <= max; i++)
@@ -21,7 +31,7 @@ namespace AdventOfCode
 
                 foreach (int nb in nbs)
                 {
-                    distance += Math.Abs(nb - i);
+                    distance += methodCalc(nb, i);
                 }
 
                 if (distance < minDistance)
@@ -30,7 +40,20 @@ namespace AdventOfCode
                 }
             }
 
-            Console.WriteLine($"First star: {minDistance}");
+            return minDistance;
         }
+
+        int CalcMethodFirstStar(int baseNb, int nb)
+        {
+            return Math.Abs(baseNb - nb);
+        }
+
+        int CalcMethodSecondStar(int baseNb, int nb)
+        {
+            int diff = Math.Abs(baseNb - nb);
+            return (diff * diff + diff) / 2;
+        }
+
+        #endregion
     }
 }
