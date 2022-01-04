8b03b8c5c72101da34e8d777ce27f2a2230bfc8b
Arthur Cousseau <arthur.cousseau@me.com>
2022-01-04 14:08:25 +0100

    Problem_2021_7: First star

---
+++ AdventOfCode/2021/Problem_2021_7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Problem_2021_7 : Problem_2021
+    {
+        public override int Number => 7;
+
+        public override void Run()
+        {
+            int[] nbs = Text.Split(',').Select(l => int.Parse(l)).ToArray();
+            int min = nbs.Min();
+            int max = nbs.Max();
+
+            int minDistance = int.MaxValue;
+
+            for (int i = min; i <= max; i++)
+            {
+                int distance = 0;
+
+                foreach (int nb in nbs)
+                {
+                    distance += Math.Abs(nb - i);
+                }
+
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                }
+            }
+
+            Console.WriteLine($"First star: {minDistance}");
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode
     {
         static void Main()
         {
-            var prob = new Problem_2021_6();
+            var prob = new Problem_2021_7();
             prob.Init();
             prob.Run();
         }
