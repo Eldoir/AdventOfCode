652199f39368cf2c558444043b1e4a17c2aeef57
Arthur Cousseau <arthur.cousseau@me.com>
2022-12-18 10:53:31 +0100

    Problem_2022_7

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode
     {
         static void Main()
         {
-            var prob = new Problem_2022_6();
+            var prob = new Problem_2022_7();
             prob.Init();
             prob.Run();
         }
