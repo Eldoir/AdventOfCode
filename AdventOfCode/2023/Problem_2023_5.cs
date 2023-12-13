e379cf71938550922fa6d54e5267fb2947f4b9a7
Arthur Cousseau <arthur.cousseau@me.com>
2023-12-13 16:58:21 +0100

    Setup problem + parse seeds

---
+++ AdventOfCode/2023/Problem_2023_5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Problem_2023_5 : Problem_2023
+    {
+        public override int Number => 5;
+
+        public override void Run()
+        {
+            UseTestInput();
+            long[] seeds = Lines[0].Split(':')[1].Trim().Split(' ').Select(long.Parse).ToArray();
+            Console.WriteLine(string.Join(",", seeds));
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode
     {
         static void Main()
         {
-            var prob = new Problem_2023_4();
+            var prob = new Problem_2023_5();
             prob.Init();
             prob.Run();
         }
