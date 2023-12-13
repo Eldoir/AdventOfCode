7c6aa4ae7a06d34ddfa14c6241ad569f8d1c9bc0
Arthur Cousseau <arthur.cousseau@me.com>
2023-12-13 18:36:39 +0100

    Parsemaps + basic implem

--- AdventOfCode/2023/Problem_2023_5.cs
+++ AdventOfCode/2023/Problem_2023_5.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode
@@ -7,11 +8,80 @@ namespace AdventOfCode
     {
         public override int Number => 5;
 
+        class Line
+        {
+            public Line(string str)
+            {
+                long[] longs = str.Split(' ').Select(long.Parse).ToArray();
+                _destinationRangeStart = longs[0];
+                _sourceRangeStart = longs[1];
+                _rangeLength = longs[2];
+            }
+
+            public long Apply(long n)
+            {
+                long delta = n - _sourceRangeStart;
+                if (delta >= 0 && delta < _rangeLength)
+                    return _destinationRangeStart + delta;
+
+                return n;
+            }
+
+            private readonly long _destinationRangeStart;
+            private readonly long _sourceRangeStart;
+            private readonly long _rangeLength;
+        }
+
+        class Map
+        {
+            public List<Line> Lines { get; } = new();
+
+            public void AddLine(Line line)
+            {
+                Lines.Add(line);
+            }
+
+            public long Apply(long n)
+            {
+                foreach (Line line in Lines)
+                {
+                    long newN = line.Apply(n);
+                    if (newN != n)
+                        return newN; // there is only one match for one number
+                }
+                return n;
+            }
+        }
+
         public override void Run()
         {
             UseTestInput();
             long[] seeds = Lines[0].Split(':')[1].Trim().Split(' ').Select(long.Parse).ToArray();
             Console.WriteLine(string.Join(",", seeds));
+            List<Map> maps = new();
+            Map currentMap = null;
+
+            for (int i = 2; i < Lines.Length; i++)
+            {
+                string line = Lines[i];
+
+                if (line.Contains("map"))
+                {
+                    currentMap = new();
+                }
+                else if (!string.IsNullOrEmpty(line))
+                {
+                    currentMap.AddLine(new Line(line));
+                }
+                else
+                {
+                    maps.Add(currentMap);
+                }
+            }
+
+            maps.Add(currentMap);
+
+            Console.WriteLine(maps.Count);
         }
     }
 }
