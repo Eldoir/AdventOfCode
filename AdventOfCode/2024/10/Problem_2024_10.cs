c973e5fb67c43fd2c0cfc3c96a1158177b422270
Arthur Cousseau <arthur.cousseau@me.com>
2024-12-19 13:08:37 +0100

    Base

---
+++ AdventOfCode/2024/10/Problem_2024_10.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode
+{
+    internal class Problem_2024_10 : Problem2
+    {
+        public override int Year => 2024;
+        public override int Number => 10;
+
+        public override long GetFirstStar()
+        {
+            return base.GetFirstStar();
+        }
+
+        protected override Test[] TestsFirstStar => new Test[]
+        {
+            new("test_1.txt", 36)
+        };
+
+        protected override Test[] TestsSecondStar => base.TestsSecondStar;
+    }
+}
