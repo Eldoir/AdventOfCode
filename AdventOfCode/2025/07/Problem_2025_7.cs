3f5dceb43f6b0b948c5e8a1369ff0076045ed7cf
Arthur Cousseau <arthur.cousseau@me.com>
2025-12-09 11:32:44 +0100

    2025_7 First star

--- AdventOfCode/2025/07/Problem_2025_7.cs
+++ AdventOfCode/2025/07/Problem_2025_7.cs
@@ -1,10 +1,70 @@
-﻿namespace AdventOfCode
+﻿#nullable enable
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
 {
     class Problem_2025_7 : Problem2
     {
         public override long GetFirstStar()
         {
-            return base.GetFirstStar();
+            int startX = Lines[0].IndexOf('S');
+            Dictionary<int, List<Splitter>> splitters = []; // indexed by X
+            for (int i = 1; i < Lines.Length; i++)
+            {
+                for (int j = 0; j < Lines[i].Length; j++)
+                {
+                    if (Lines[i][j] == '^')
+                    {
+                        Splitter splitter = new(j, i);
+
+                        if (!splitters.ContainsKey(splitter.X))
+                            splitters.Add(splitter.X, []);
+
+                        splitters[j].Add(splitter);
+                    }
+                }
+            }
+            Stack<(int X, int Y)> beams = [];
+            beams.Push((startX, 0));
+            int uniqueHits = 0;
+            HashSet<(int X, int Y)> visited = [];
+            while (beams.Count > 0)
+            {
+                (int beamX, int beamY) = beams.Pop();
+                if (visited.Contains((beamX, beamY))) continue;
+                visited.Add((beamX, beamY));
+                if (!splitters.ContainsKey(beamX)) continue; // no splitter on this column
+                Splitter? splitter = splitters[beamX].FirstOrDefault(splitter => splitter.Y > beamY);
+                if (splitter is null) continue; // no splitter below the beam on this column
+                if (!splitter.Hit)
+                {
+                    splitter.SetHit();
+                    uniqueHits++;
+                }
+                if (beamX > 0) beams.Push((beamX - 1, splitter.Y));
+                if (beamX < Lines[0].Length - 1) beams.Push((beamX + 1, splitter.Y));
+            }
+            return uniqueHits;
+        }
+
+        class Splitter
+        {
+            public int X { get; }
+            public int Y { get; }
+            public bool Hit { get; private set; }
+
+            public Splitter(int x, int y)
+            {
+                X = x;
+                Y = y;
+                Hit = false;
+            }
+            
+            public void SetHit()
+            {
+                Hit = true;
+            }
         }
 
         protected override Test[] TestsFirstStar =>
