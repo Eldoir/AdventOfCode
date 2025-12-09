38121b9bc07d24091cb1b6e0467df2d12e4b3a26
Arthur Cousseau <arthur.cousseau@me.com>
2025-12-09 15:43:12 +0100

    2025_7 prepare second star

--- AdventOfCode/2025/07/Problem_2025_7.cs
+++ AdventOfCode/2025/07/Problem_2025_7.cs
@@ -8,6 +8,33 @@ namespace AdventOfCode
     {
         public override long GetFirstStar()
         {
+            ProblemData problem = Parse();
+            Stack<(int X, int Y)> beams = [];
+            beams.Push((problem.StartX, 0));
+            int uniqueHits = 0;
+            while (beams.Count > 0)
+            {
+                (int beamX, int beamY) = beams.Pop();
+                if (!problem.Splitters.ContainsKey(beamX)) continue; // no splitter on this column
+                Splitter? splitter = problem.Splitters[beamX].FirstOrDefault(splitter => splitter.Y > beamY);
+                if (splitter is null || splitter.Hit) continue; // no splitter below the beam on this column, or it's already hit
+                splitter.SetHit();
+                uniqueHits++;
+                if (beamX > 0) beams.Push((beamX - 1, splitter.Y));
+                if (beamX < Lines[0].Length - 1) beams.Push((beamX + 1, splitter.Y));
+            }
+            return uniqueHits;
+        }
+
+        public override long GetSecondStar()
+        {
+            ProblemData problem = Parse();
+
+            return base.GetSecondStar();
+        }
+
+        ProblemData Parse()
+        {
             int startX = Lines[0].IndexOf('S');
             Dictionary<int, List<Splitter>> splitters = []; // indexed by X
             for (int i = 1; i < Lines.Length; i++)
@@ -25,22 +52,10 @@ namespace AdventOfCode
                     }
                 }
             }
-            Stack<(int X, int Y)> beams = [];
-            beams.Push((startX, 0));
-            int uniqueHits = 0;
-            while (beams.Count > 0)
-            {
-                (int beamX, int beamY) = beams.Pop();
-                if (!splitters.ContainsKey(beamX)) continue; // no splitter on this column
-                Splitter? splitter = splitters[beamX].FirstOrDefault(splitter => splitter.Y > beamY);
-                if (splitter is null || splitter.Hit) continue; // no splitter below the beam on this column, or it's already hit
-                splitter.SetHit();
-                uniqueHits++;
-                if (beamX > 0) beams.Push((beamX - 1, splitter.Y));
-                if (beamX < Lines[0].Length - 1) beams.Push((beamX + 1, splitter.Y));
-            }
-            return uniqueHits;
+            return new ProblemData(startX, splitters);
         }
+
+        record class ProblemData(int StartX, Dictionary<int, List<Splitter>> Splitters);
 
         class Splitter
         {
@@ -65,5 +80,10 @@ namespace AdventOfCode
         [
             new Test("example", 21)
         ];
+
+        protected override Test[] TestsSecondStar =>
+       [
+           new Test("example", 40)
+       ];
     }
 }
